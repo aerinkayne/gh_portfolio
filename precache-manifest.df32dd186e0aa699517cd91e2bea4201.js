0b1334ba77c0f92022a31dbb6da147b70beffd8a
Carrie <chanscom7@gmail.com>
2020-10-01 16:23:14 -0400

    Updates

---
+++ precache-manifest.df32dd186e0aa699517cd91e2bea4201.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b06fb165c923c09e2f228a63559e378e",
+    "url": "/gh_portfolio/index.html"
+  },
+  {
+    "revision": "e1d3f89ce1a7d5b2ad67",
+    "url": "/gh_portfolio/static/css/main.e02d05cb.chunk.css"
+  },
+  {
+    "revision": "6246b715fb1ece58c488",
+    "url": "/gh_portfolio/static/js/2.27e12423.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/gh_portfolio/static/js/2.27e12423.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1d3f89ce1a7d5b2ad67",
+    "url": "/gh_portfolio/static/js/main.e41ef651.chunk.js"
+  },
+  {
+    "revision": "c7894c1bc3f574ee1923",
+    "url": "/gh_portfolio/static/js/runtime-main.4a44cc9b.js"
+  },
+  {
+    "revision": "49899bded3feef9b1f04ef9dd0b58dfc",
+    "url": "/gh_portfolio/static/media/blueSqr3.49899bde.jpg"
+  },
+  {
+    "revision": "febf7de7a261a0fe322ec21cca67f68e",
+    "url": "/gh_portfolio/static/media/blue_purp_flow.febf7de7.jpg"
+  },
+  {
+    "revision": "912de1bb1801937c99c681a4c4dd9558",
+    "url": "/gh_portfolio/static/media/colorGlass.912de1bb.jpg"
+  },
+  {
+    "revision": "5b6b867985471badf10fd7f062d308b4",
+    "url": "/gh_portfolio/static/media/falconEdit.5b6b8679.png"
+  },
+  {
+    "revision": "9eb094e9cf1c331535769229e01f8b43",
+    "url": "/gh_portfolio/static/media/fallMaple_edit_589840.9eb094e9.jpeg"
+  },
+  {
+    "revision": "a346ee68f8f47648b3bf61c60c9df165",
+    "url": "/gh_portfolio/static/media/flame.a346ee68.jpg"
+  },
+  {
+    "revision": "ac3f360b8b9ee7c8441e04a5d072f406",
+    "url": "/gh_portfolio/static/media/forest-sunbeams-trees-sunlight-70365.ac3f360b.jpeg"
+  },
+  {
+    "revision": "14437e3379ebb78fd6ae096bfe3736d2",
+    "url": "/gh_portfolio/static/media/jellyCr1076758.14437e33.jpg"
+  },
+  {
+    "revision": "f6d4401da8c4ac9bf9ead747d70eb228",
+    "url": "/gh_portfolio/static/media/macawEdit.f6d4401d.png"
+  },
+  {
+    "revision": "a74a7259d08f8c6de49f0a30de571b72",
+    "url": "/gh_portfolio/static/media/macbook-pro-131187negativespace.a74a7259.jpg"
+  },
+  {
+    "revision": "26400e7684538d8eaa00cd767f31ab15",
+    "url": "/gh_portfolio/static/media/matrixy.26400e76.jpg"
+  },
+  {
+    "revision": "b940e3ac40a9ead1339715fcde9213c0",
+    "url": "/gh_portfolio/static/media/paintBird.b940e3ac.jpg"
+  },
+  {
+    "revision": "8aa15ca56ca72c577f874ea27bc758d9",
+    "url": "/gh_portfolio/static/media/pexels-photo-1597662chuanyu1400.8aa15ca5.jpeg"
+  },
+  {
+    "revision": "b164e86db837a3391ed8b4e58cdc898f",
+    "url": "/gh_portfolio/static/media/pexels-photo-235621pixabay.b164e86d.jpeg"
+  },
+  {
+    "revision": "11572a3e9484b881f7e3754325937ba4",
+    "url": "/gh_portfolio/static/media/pexels-photo-235985pixabay.11572a3e.jpeg"
+  },
+  {
+    "revision": "e7b35c6e09a90d76bf478e2d9bd39612",
+    "url": "/gh_portfolio/static/media/pexels-photo-262713crop.e7b35c6e.jpeg"
+  },
+  {
+    "revision": "c88c6ef0dd4795f9d9aa0d24664c699b",
+    "url": "/gh_portfolio/static/media/pexels-photo-41102skitterphoto.c88c6ef0.jpeg"
+  },
+  {
+    "revision": "2027d70f6f582f96a31e7619bacdf3ab",
+    "url": "/gh_portfolio/static/media/pexelsEdit_2253391.2027d70f.png"
+  },
+  {
+    "revision": "d55a76b869e9c4db13d838a51eaabd3d",
+    "url": "/gh_portfolio/static/media/pine_yellowUnder_cut_247421.d55a76b8.jpeg"
+  },
+  {
+    "revision": "5a73a26c34e7b496bfb87e9b01973774",
+    "url": "/gh_portfolio/static/media/redCrabCr2289652.5a73a26c.jpg"
+  },
+  {
+    "revision": "bcab0435f8f33e8b9f7c4674ac872472",
+    "url": "/gh_portfolio/static/media/sandReeds1300510sm.bcab0435.jpg"
+  },
+  {
+    "revision": "ce4c66340b5a49834c5742db6f2f4fc1",
+    "url": "/gh_portfolio/static/media/sharkCr2_67854.ce4c6634.jpg"
+  },
+  {
+    "revision": "9ca1facffdbbbd847836b8f8c2d6b77c",
+    "url": "/gh_portfolio/static/media/shore_rocks_Cr414320.9ca1facf.jpg"
+  },
+  {
+    "revision": "fbed6854f96455454060f3c02700400f",
+    "url": "/gh_portfolio/static/media/trees-in-forest-during-sunset-247421.fbed6854.jpg"
+  },
+  {
+    "revision": "981b0bcd97ee60f1f8e1609a12610de9",
+    "url": "/gh_portfolio/static/media/turtle2765872.981b0bcd.jpeg"
+  },
+  {
+    "revision": "bd88ac1fefb8a4da26debffbd3ebab6e",
+    "url": "/gh_portfolio/static/media/water_rocksCrT590178.bd88ac1f.jpg"
+  }
+]);
